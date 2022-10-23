e35439f89d2c05276137f56ff649576270d8f3e0
Pankei <studypankeev@gmail.com>
2022-10-23 22:17:21 +0400

    I wrote the c_f function, which receives the folder name as input. The function performs the following work: using the conditional if operator and the logical not operator, which finds out if there is no dataset folder using the os.path nested module with the isdir function, checks whether there is a folder with this name, if not, the next mkdir operator creates a directory named dataset.The following conditional logical operator checks whether the specified path exists (the join operator connects paths), if not, then creates this path using mkdir.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import os
 import bs4 import BeautifulSoup
+
+
+def creating_folder(folder_name):
+    if not os.path.isdir('dataset'):
+        os.mkdir('dataset')
+    if not os.path.exists(os.path.join('dataset',folder_name)):
+        os.mkdir(os.path.join('dataset',folder_name))
