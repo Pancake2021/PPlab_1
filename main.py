1c267c968bbdc9000d0a7c5591c78c683229af4d
Pankei <studypankeev@gmail.com>
2022-10-24 16:25:06 +0400

    I wrote a function with which we will create a link to the image. We start a cycle with a counter from 1 to 35. These are the page numbers on Yandex pictures, since there are about 20 images on one page, so there will be about 700 on 35. Output the page number. The following function finds all spaces in the link text and changes them to %20. Next, we create a variable link with changeable parameters. Let's create a response variable. Let's connect the requests library. Now we will get text-type content, namely a link using a browser and a website. But the browser bans after some time, because with this software we commit "DDoS". Therefore, we will add a header that creates an imitation of what we enter from the browser. Let's add a system pause command in 2 seconds. We use the soup variable together with this, we will connect the parser using the lxml library. The image_control variable parses images in the html code of the page belonging to the specified class. We run a counter that will be passed through the images and change the link well the side we need

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import os
-import bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
+import requests
+import time
+import lxml
 
+header = {
+    'Connection': 'keep-alive',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 '
+                  'Safari/537.36 OPR/40.0.2308.81',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'DNT': '1',
+    'Accept-Encoding': 'gzip, deflate, lzma, sdch',
+    'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4'
+}
 
 def creating_folder(folder_name):
     if not os.path.isdir('dataset'):
         os.mkdir('dataset')
     if not os.path.exists(os.path.join('dataset',folder_name)):
         os.mkdir(os.path.join('dataset',folder_name))
+
+def creating_url(request_name):
+    for page_number in range(1,35):
+        print(page_number, "page")
+        request_name.replace('','%20')
+        link = f'https://yandex.ru/images/search?text={request_name}&p={page_number}'
+        response = requests.get(link, headers=header).text
+        sleep(2)
+        soup = BeautifulSoup(response, 'lxml')
+        image_control = soup.find_all('img', class_='serp-item__thumb justifier__thumb')
+        for image in image_control:
+            image_link = 'https:' + image.get('src')
+            print(image_link)
+            yield (image_link)  # итератор
